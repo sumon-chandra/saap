fc40cb686e16efa926e780778289a8719d438ba6
Sumon Chandra <topu199041@gmail.com>
2023-11-30 16:53:52 +0600

    remove .env

--- components/authForm/AuthForm.tsx
+++ components/authForm/AuthForm.tsx
@@ -61,20 +61,55 @@ const AuthForm = ({ isOpen, onClose }: AuthFormProps) => {
     }
 
     return (
-        <Modal backdrop='blur' isOpen={isOpen} onClose={onClose} isDismissable={false} size='3xl'>
+        <Modal
+            backdrop='blur'
+            isOpen={isOpen}
+            onClose={onClose}
+            isDismissable={false}
+            size='3xl'
+        >
             <ModalContent className='py-4'>
                 {(onClose) => (
                     <>
-                        <ModalHeader className='flex flex-col items-center justify-center w-full text-3xl'>{variant === "REGISTER" ? "Register" : "Login"} to SAAP</ModalHeader>
+                        <ModalHeader
+                            className='flex flex-col items-center justify-center w-full text-3xl'
+                        >
+                            {variant === "REGISTER" ? "Register" : "Login"} to SAAP
+                        </ModalHeader>
                         <ModalBody className='w-full mx-auto md:w-1/2'>
                             <form onSubmit={handleSubmit(onSubmit)} >
                                 {variant === "REGISTER" && (
-                                    <Input label='Name' type="text" errors={errors} id="name" register={register} />
+                                    <Input
+                                        label='Name'
+                                        type="text"
+                                        errors={errors}
+                                        id="name"
+                                        register={register}
+                                    />
                                 )}
-                                <Input label='Email' register={register} errors={errors} id='email' type='email' />
-                                <Input label='Password' register={register} errors={errors} id='password' type='password' />
+                                <Input
+                                    label='Email'
+                                    register={register}
+                                    errors={errors}
+                                    id='email'
+                                    type='email'
+                                />
+                                <Input
+                                    label='Password'
+                                    register={register}
+                                    errors={errors}
+                                    id='password'
+                                    type='password'
+                                />
                                 <div className='w-full mt-4'>
-                                    <Button type="submit" color='success' className='w-full font-bold text-white' disabled={isLoading}>{variant === "LOGIN" ? "Login" : "Register"}</Button>
+                                    <Button
+                                        type="submit"
+                                        color='success'
+                                        className='w-full font-bold text-white'
+                                        disabled={isLoading}
+                                    >
+                                        {variant === "LOGIN" ? "Login" : "Register"}
+                                    </Button>
                                 </div>
                             </form>
                             <div className='flex items-center justify-center gap-5 mt-5'>
@@ -83,8 +118,14 @@ const AuthForm = ({ isOpen, onClose }: AuthFormProps) => {
                                 <Divider orientation='horizontal' className='w-20' />
                             </div>
                             <div className='mt-5 space-y-2'>
-                                <SocialAuthButton icon={BsGoogle} onClick={() => socialAction("google")} />
-                                <SocialAuthButton icon={BsFacebook} onClick={() => socialAction("facebook")} />
+                                <SocialAuthButton
+                                    icon={BsGoogle}
+                                    onClick={() => socialAction("google")}
+                                />
+                                <SocialAuthButton
+                                    icon={BsFacebook}
+                                    onClick={() => socialAction("facebook")}
+                                />
 
                             </div>
                             <div className="flex justify-center gap-2 px-2 mt-6 text-sm text-gray-500">
