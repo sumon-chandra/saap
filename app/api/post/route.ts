46a177ad6e586a05d9612d850acc0526c50ea94e
Sumon Chandra <topu199041@gmail.com>
2023-12-05 22:31:50 +0600

    chore: create POST method for post

---
+++ app/api/post/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import getLoggedUser from "@/app/actions/getLoggedUser";
+import prisma from "@/lib/prismadb";
+
+export async function POST(request: Request) {
+    try {
+        const responseBody = await request.json()
+        const { body, image } = responseBody;
+        const user = await getLoggedUser()
+
+        if (!body) {
+            return new NextResponse("Post's cannot be empty!!", { status: 401 })
+        }
+
+        if (!user) {
+            return new NextResponse("Failed to detect author!!", { status: 402 })
+        }
+
+        const post = await prisma.post.create({
+            data: {
+                body,
+                image,
+                userId: user.id
+            },
+            include: {
+                likes: true
+            }
+        })
+
+        return NextResponse.json(post)
+    } catch (error: any) {
+        console.log("ERROR_WHILE_POST", error);
+        return new NextResponse("Internal Server Error, while Posting!! Please try again", { status: 500 })
+    }
+}
