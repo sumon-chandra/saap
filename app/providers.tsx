c06b456ecd00b4a15aed3f9d957921dd79e3db57
Sumon Chandra <topu199041@gmail.com>
2023-12-05 23:53:10 +0600

    config: configure react-query and uploadthing

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,20 +5,27 @@ import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export interface ProvidersProps {
 	children: React.ReactNode;
 	themeProps?: ThemeProviderProps;
 }
 
+const queryClient = new QueryClient()
+
 export function Providers({ children, themeProps }: ProvidersProps) {
 	return (
 		<NextUIProvider>
-			<NextThemesProvider {...themeProps}>
-				<SessionProvider>
-					{children}
-				</SessionProvider>
-			</NextThemesProvider>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
+				<NextThemesProvider {...themeProps}>
+					<SessionProvider>
+						{children}
+					</SessionProvider>
+				</NextThemesProvider>
+			</QueryClientProvider>
 		</NextUIProvider>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-import { nextui } from '@nextui-org/theme'
+const { nextui } = require("@nextui-org/react");
+import { withUt } from "uploadthing/tw";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withUt({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -29,4 +30,4 @@ module.exports = {
     nextui(),
     require("@tailwindcss/forms")({ strategy: "class" })
   ],
-}
+})
