d0ba53046f839a8d9e766534e04fce1223375489
Sumon Chandra <topu199041@gmail.com>
2023-12-08 20:50:56 +0600

    refactor: add skeleton on nav

--- components/leftSidebar/nav/NavList.tsx
+++ components/leftSidebar/nav/NavList.tsx
@@ -1,15 +1,59 @@
-import { siteConfig } from "@/config/site"
+"use client"
+import { FaHome, FaUser, FaHashtag, FaBookmark } from "react-icons/fa";
+import { TbMail } from "react-icons/tb";
 import NavItem from "./NavItem"
+import { useSession } from "next-auth/react"
+
+const Skeleton = ({ key }: { key: string }) => {
+    return (
+        <div key={key} className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-default-200 dark:bg-saap-bg-dark-primary py-6 w-full"></div>
+        </div>
+    )
+}
 
 const NavList = () => {
+    const { status, data } = useSession()
+    const navItems = [
+        {
+            label: "Home",
+            href: "/",
+            icon: FaHome
+        },
+        {
+            label: "Profile",
+            href: `/profile/${data?.user?.userName ? data?.user?.userName : data?.user?.id}`,
+            icon: FaUser
+        },
+        {
+            label: "Topics",
+            href: "/topics",
+            icon: FaHashtag
+        },
+        {
+            label: "Saved",
+            href: "/saved",
+            icon: FaBookmark
+        },
+        {
+            label: "Invite Friends",
+            href: "/invite-friends",
+            icon: TbMail
+        }
+    ]
+
     return (
         <nav className="flex flex-col gap-4 p-4">
-            {siteConfig.navItems.map(navItem => (
+            {status === "loading" ? (
+                navItems.map(item => (
+                    <Skeleton key={item.label} />
+                ))
+            ) : navItems.map(item => (
                 <NavItem
-                    key={navItem.label}
-                    href={navItem.href}
-                    label={navItem.label}
-                    Icon={navItem.icon}
+                    key={item.label}
+                    Icon={item.icon}
+                    label={item.label}
+                    href={item.href}
                 />
             ))}
         </nav>
