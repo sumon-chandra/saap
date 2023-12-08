b54e099b255e4bf8c737eb1143306ea827efe093
Sumon Chandra <topu199041@gmail.com>
2023-12-08 21:08:06 +0600

    Refactor: add skeleton on left side header

--- components/leftSidebar/Header.tsx
+++ components/leftSidebar/Header.tsx
@@ -4,14 +4,23 @@ import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { FaUser } from 'react-icons/fa';
 
+const Skeleton = () => {
+    return (
+        <div className="animate-pulse space-y-1 w-full">
+            <div className="rounded-full bg-default-200 dark:bg-saap-bg-dark-primary h-4 w-full"></div>
+            <div className="rounded-full bg-default-200 dark:bg-saap-bg-dark-primary h-3 py-1 w-1/2"></div>
+        </div>
+    )
+}
+
 const Header = () => {
     const { data: session, status } = useSession()
     // console.log({ session, status });
-    const username = session?.user.userName || session?.user.id.slice(0, 10)
+    const username = session?.user.userName || `@${session?.user.id.slice(0, 10)}`
 
     return (
         <header className="space-y-4">
-            <Link href="/profile/username">
+            <Link href={`/profile/${username}`}>
                 <div className='flex items-start justify-start gap-2'>
                     <div>
                         <Avatar
@@ -22,10 +31,16 @@ const Header = () => {
                             {!session?.user?.image || <FaUser />}
                         </Avatar>
                     </div>
-                    <div className='flex flex-col items-start'>
-                        <h3 className='text-[16px] font-bold leading-5 text-saap-text-primary dark:text-saap-text-dark-primary'>{session?.user?.name}</h3>
-                        <h5 className='text-sm font-semibold text-saap-text-secondary dark:text-saap-text-dark-secondary'>@{username}</h5>
-                    </div>
+                    {status === "loading" ? (
+                        <Skeleton />
+                    ) : (
+                        <>
+                            <div className='flex flex-col items-start'>
+                                <h3 className='text-[16px] font-bold leading-5 text-saap-text-primary dark:text-saap-text-dark-primary'>{session?.user?.name}</h3>
+                                <h5 className='text-sm font-semibold text-saap-text-secondary dark:text-saap-text-dark-secondary'>{username}</h5>
+                            </div>
+                        </>
+                    )}
                 </div>
             </Link>
             <div className='flex items-center justify-between'>
