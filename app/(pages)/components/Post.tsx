c135f77947cc68345ce1faaca963c631894f81d8
Sumon Chandra <topu199041@gmail.com>
2023-12-23 11:47:49 +0600

    build: create post footer

--- app/(pages)/components/Post.tsx
+++ app/(pages)/components/Post.tsx
@@ -1,19 +1,31 @@
 "use client"
-import { Card, CardBody, CardHeader } from "@nextui-org/react"
-import { Post } from "@prisma/client"
 import Image from "next/image"
-import { FC } from "react"
+import { Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react"
+import { Post } from "@prisma/client"
+import { FC, useState } from "react"
+import { FaHeart, FaRegHeart, FaComment, FaRegComment, FaShareSquare } from "react-icons/fa";
+import clsx from "clsx"
+
 interface PostProps {
     post: Post
 }
 
 const SinglePost: FC<PostProps> = ({ post }) => {
+    const [isLiked, setIsLiked] = useState(true)
+    const [isCommented, setIsCommented] = useState(false)
+
+    const like = 2
+    const comment = 12
+    const share = 12
+
     return (
-        <Card className="w-full dark:bg-saap-bg-dark-primary">
+        <Card
+            className="w-full shadow-none rounded  bg-saap-bg-primary dark:bg-saap-bg-dark-secondary"
+        >
             <CardHeader>
             </CardHeader>
             <CardBody>
-                <div className=" flex flex-col gap-4">
+                <div className="flex flex-col gap-4">
                     <div>
                         <p>{post.body}</p>
                     </div>
@@ -30,6 +42,29 @@ const SinglePost: FC<PostProps> = ({ post }) => {
 
                 </div>
             </CardBody>
+            <Divider />
+            <CardFooter className="flex items-center justify-start gap-6">
+                <div
+                    className={clsx("flex items-center gap-2 px-3 py-1 rounded-full cursor-pointer",
+                        isLiked && "bg-saap-bg-primary-light text-saap-secondary dark:bg-saap-bg-dark-primary"
+                    )}
+                >
+                    {isLiked ? <FaHeart /> : <FaRegHeart />}
+                    {like > 0 && <div>{like}</div>}
+                </div>
+                <div
+                    className={clsx("flex items-center gap-2 px-3 py-1 rounded-full cursor-pointer",
+                        isCommented && "bg-saap-bg-primary-light text-saap-secondary dark:bg-saap-bg-dark-primary"
+                    )}
+                >
+                    {isCommented ? <FaComment /> : <FaRegComment />}
+                    {comment > 0 && <div>{comment}</div>}
+                </div>
+                <div className="flex items-center gap-2 px-3 py-1 rounded-full cursor-pointer">
+                    <FaShareSquare />
+                    {share > 0 && <div>{share}</div>}
+                </div>
+            </CardFooter>
         </Card>
     )
 }
