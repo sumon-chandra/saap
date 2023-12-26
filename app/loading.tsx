39262a8ac0b916264a0ee4610fd67a5172860f49
Sumon Chandra <topu199041@gmail.com>
2023-12-26 20:25:18 +0600

    refactor: change root loading content

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,31 +1,8 @@
-import Image from 'next/image';
-import React from 'react';
-
 export default function RootLoading() {
-   const LogoImage = '/logo.png';
-   //  const MetaImage = '/meta.png';
-
    return (
-      <div className="flex flex-col justify-between h-screen ">
+      <div className="flex flex-col justify-between h-screen select-none">
          <div className="flex-grow flex items-center justify-center ">
-            <Image
-               alt="Threads"
-               src={LogoImage}
-               width={100}
-               height={100}
-               priority
-            />
-         </div>
-         <div className='w-full  flex justify-center items-center mb-6'>
-            <Image
-               priority
-               alt="Meta"
-               //   src={MetaImage}
-               src={LogoImage}
-               width={90}
-               height={90}
-               className='w-auto'
-            />
+            <h1 className='text-4xl font-black text-saap-primary italic'>Saap.</h1>
          </div>
       </div>
    );
