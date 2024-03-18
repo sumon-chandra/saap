2d42da41de94e19e10ac608efa58a9eca0ea43d4
Sumon Chandra <topu199041@gmail.com>
2024-03-18 20:59:43 +0600

    fix: change the register route prisma file path

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,37 +1,43 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import prisma from "@/lib/prismadb";
-import { generateUserName } from "@/utils/generateUsername";
+import prisma from "../../../lib/prismadb";
+import { generateUserName } from "../../../utils/generateUsername";
 
 export async function POST(request: Request) {
-    try {
-        const body = await request.json();
+	try {
+		const body = await request.json();
 
-        const { name, email, password } = body;
+		const { name, email, password } = body;
 
-        if (!email || !password || !name) {
-            return new NextResponse("Invalid Credentials!!", { status: 401 })
-        }
+		if (!email || !password || !name) {
+			return new NextResponse("Invalid Credentials!!", {
+				status: 401,
+			});
+		}
 
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const existingUser = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email,
+			},
+		});
 
-        if (existingUser?.email === email) {
-            return new NextResponse("User already exists!!", { status: 409 })
-        }
-        const userName = generateUserName(email)
+		if (existingUser?.email === email) {
+			return new NextResponse("User already exists!!", {
+				status: 409,
+			});
+		}
+		const userName = generateUserName(email);
 
-        const user = await prisma.user.create({
-            data: { name, email, hashedPassword, userName }
-        })
+		const user = await prisma.user.create({
+			data: { name, email, hashedPassword, userName },
+		});
 
-        return NextResponse.json(user)
-    } catch (error: any) {
-        console.log("REGISTRATION_ERROR", error);
-        return new NextResponse("Registration failed!!", { status: 500 })
-    }
+		return NextResponse.json(user);
+	} catch (error: any) {
+		console.log("REGISTRATION_ERROR", error);
+		return new NextResponse("Registration failed!!", {
+			status: 500,
+		});
+	}
 }
