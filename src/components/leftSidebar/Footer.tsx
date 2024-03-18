77fed74812b7bc4e1ecbbc5e42a5211887b41869
Sumon Chandra <topu199041@gmail.com>
2024-03-18 21:24:15 +0600

    fix:  replace file paths

--- src/components/leftSidebar/Footer.tsx
+++ src/components/leftSidebar/Footer.tsx
@@ -1,53 +1,59 @@
-"use client"
-import { ThemeSwitch } from "@/components/theme-switch";
-import { FiHelpCircle, FiSettings } from "react-icons/fi"
-import { Button, Tooltip } from '@nextui-org/react';
+"use client";
+import { ThemeSwitch } from "../../components/theme-switch";
+import { FiHelpCircle, FiSettings } from "react-icons/fi";
+import { Button, Tooltip } from "@nextui-org/react";
 import { MdLogout } from "react-icons/md";
 import { signOut, useSession } from "next-auth/react";
 
 const Footer = () => {
-    const { status } = useSession()
-    const handleLogout = () => {
-        signOut({ redirect: false })
-    }
-    return (
-        <div className="absolute bottom-0 flex items-center justify-between w-full py-5 border-t">
-            <Tooltip showArrow content="Switch theme">
-                <Button
-                    size="sm"
-                    isIconOnly
-                    className='flex items-center justify-center p-0 transition-all bg-transparent rounded-full'>
-                    <ThemeSwitch />
-                </Button>
-            </Tooltip>
-            <Tooltip showArrow content="Settings">
-                <Button
-                    size="sm"
-                    isIconOnly
-                    className='flex items-center justify-center p-0 transition-all bg-transparent rounded-full'>
-                    <FiSettings className="text-xl" />
-                </Button>
-            </Tooltip>
-            <Tooltip showArrow content="Help">
-                <Button
-                    size="sm"
-                    isIconOnly
-                    className='flex items-center justify-center p-0 transition-all bg-transparent rounded-full'>
-                    <FiHelpCircle className="text-xl" />
-                </Button>
-            </Tooltip>
-            <Tooltip showArrow content="Logout">
-                <Button
-                    onClick={handleLogout}
-                    size="sm"
-                    disabled={status === "loading"}
-                    isIconOnly
-                    className='flex items-center justify-center p-0 transition-all bg-transparent rounded-full disabled:opacity-50 disabled:cursor-not-allowed'>
-                    <MdLogout className="text-xl" />
-                </Button>
-            </Tooltip>
-        </div>
-    )
-}
+	const { status } = useSession();
+	const handleLogout = () => {
+		signOut({ redirect: false });
+	};
+	return (
+		<div className="absolute bottom-0 flex items-center justify-between w-full py-5 border-t">
+			<Tooltip showArrow content="Switch theme">
+				<Button
+					size="sm"
+					isIconOnly
+					className="flex items-center justify-center p-0 transition-all bg-transparent rounded-full"
+				>
+					<ThemeSwitch />
+				</Button>
+			</Tooltip>
+			<Tooltip showArrow content="Settings">
+				<Button
+					size="sm"
+					isIconOnly
+					className="flex items-center justify-center p-0 transition-all bg-transparent rounded-full"
+				>
+					<FiSettings className="text-xl" />
+				</Button>
+			</Tooltip>
+			<Tooltip showArrow content="Help">
+				<Button
+					size="sm"
+					isIconOnly
+					className="flex items-center justify-center p-0 transition-all bg-transparent rounded-full"
+				>
+					<FiHelpCircle className="text-xl" />
+				</Button>
+			</Tooltip>
+			<Tooltip showArrow content="Logout">
+				<Button
+					onClick={handleLogout}
+					size="sm"
+					disabled={
+						status === "loading"
+					}
+					isIconOnly
+					className="flex items-center justify-center p-0 transition-all bg-transparent rounded-full disabled:opacity-50 disabled:cursor-not-allowed"
+				>
+					<MdLogout className="text-xl" />
+				</Button>
+			</Tooltip>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
--- src/components/rightSidebar/RightSidebar.tsx
+++ src/components/rightSidebar/RightSidebar.tsx
@@ -1,36 +1,45 @@
-import { siteConfig } from "@/config/site";
+import { siteConfig } from "../../config/site";
 import NextLink from "next/link";
 import { FaUser } from "react-icons/fa";
 const RightSidebar = () => {
-     return (
-          <aside className="sticky  top-0 w-[250px] right-0 hidden lg:flex h-[100dvh] flex-col gap-10 p-4 bg-saap-bg-primary dark:bg-saap-bg-dark-secondary rounded-lg shadow-lg">
-               <header className="flex items-center justify-start gap-1">
-                    <div>
-                         <FaUser />
-                    </div>
-                    <div>
-                         <h3>Sumon Chandra</h3>
-                    </div>
-               </header>
-               <nav className="flex flex-col gap-4">
-                    {siteConfig.navItems.map(navItem => (
-                         <NextLink href={navItem.href} key={navItem.label}>
-                              {navItem.label}
-                         </NextLink>
-                    ))}
-                    {siteConfig.navItems.map(navItem => (
-                         <NextLink href={navItem.href} key={navItem.label}>
-                              {navItem.label}
-                         </NextLink>
-                    ))}
-                    {siteConfig.navItems.map(navItem => (
-                         <NextLink href={navItem.href} key={navItem.label}>
-                              {navItem.label}
-                         </NextLink>
-                    ))}
-               </nav>
-          </aside>
-     );
+	return (
+		<aside className="sticky  top-0 w-[250px] right-0 hidden lg:flex h-[100dvh] flex-col gap-10 p-4 bg-saap-bg-primary dark:bg-saap-bg-dark-secondary rounded-lg shadow-lg">
+			<header className="flex items-center justify-start gap-1">
+				<div>
+					<FaUser />
+				</div>
+				<div>
+					<h3>Sumon Chandra</h3>
+				</div>
+			</header>
+			<nav className="flex flex-col gap-4">
+				{siteConfig.navItems.map((navItem) => (
+					<NextLink
+						href={navItem.href}
+						key={navItem.label}
+					>
+						{navItem.label}
+					</NextLink>
+				))}
+				{siteConfig.navItems.map((navItem) => (
+					<NextLink
+						href={navItem.href}
+						key={navItem.label}
+					>
+						{navItem.label}
+					</NextLink>
+				))}
+				{siteConfig.navItems.map((navItem) => (
+					<NextLink
+						href={navItem.href}
+						key={navItem.label}
+					>
+						{navItem.label}
+					</NextLink>
+				))}
+			</nav>
+		</aside>
+	);
 };
 
 export default RightSidebar;
